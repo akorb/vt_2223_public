241a59cf8c4d097c493786788ebb94ed9690c39d
Andreas Korb <andreas.d.korb@gmail.com>
2022-11-06 16:17:48 +0100

    Add simple interpreter solution

--- hw02/simple.c
+++ hw02/simple.c
@@ -1,7 +1,104 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 #include "gen.h"
 
+typedef struct {
+    uint32_t ip;
+    int32_t accumulator;
+    int32_t loop_counter;
+} machine_state_t;
+
+// Changing default values of enum constants
+enum instruction {
+    HALT = 0,
+    CLRA = 1,
+    INC3A = 2,
+    DECA = 3,
+    SETL = 4,
+    BACK7 = 5,
+};
+
+typedef unsigned char instruction_t;
+
+machine_state_t machineState = { 0 };
+int halt_machine = 0;
+
+void halt() {
+    halt_machine = 1;
+    machineState.ip++;
+}
+
+void clrA() {
+    machineState.accumulator = 0;
+    machineState.ip++;
+}
+
+void inc3A() {
+    machineState.accumulator += 3;
+    machineState.ip++;
+}
+
+void decA() {
+    machineState.accumulator--;
+    machineState.ip++;
+}
+
+void setL() {
+    machineState.loop_counter = machineState.accumulator;
+    machineState.ip++;
+}
+
+void back7() {
+    machineState.loop_counter--;
+    if (machineState.loop_counter >= 0) {
+        machineState.ip -= 7;
+    } else {
+        machineState.ip++;
+    }
+}
+
+int main_loop(const char* code) {
+    instruction_t inst;
+    while (!halt_machine) {
+        inst = code[machineState.ip];
+        switch (inst) {
+            case HALT:
+                halt();
+                break;
+            case CLRA:
+                clrA();
+                break;
+            case INC3A:
+                inc3A();
+                break;
+            case DECA:
+                decA();
+                break;
+            case SETL:
+                setL();
+                break;
+            case BACK7:
+                back7();
+                break;
+            default:
+                puts("Should not happen!");
+                exit(1);
+        }
+    }
+}
+
 int main() {
-    printf("Hello, World!\n");
+    int prob[] = {0, 1, 0, 0, 0};
+    int32_t zero = 0;
+    char code[10000];
+    init(code, 10000, prob, 1, &zero, &zero);
+
+    main_loop(code);
+
+    printf("Final state:\n"
+           "ip: %u\n"
+           "A: %i\n"
+           "L: %i\n", machineState.ip, machineState.accumulator, machineState.loop_counter);
     return 0;
 }
